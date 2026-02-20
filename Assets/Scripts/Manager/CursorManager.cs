68ca9c511f172faa2af573b28fd207b11ea95667
level1pr0grammer <thaaun05@gmail.com>
2026-02-20 14:23:15 +0700

    Add Mouse Lock Press G

---
+++ Assets/Scripts/Manager/CursorManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorManager : MonoBehaviour
+{
+    private bool isCursorLocked = true;
+
+    void Start()
+    {
+        LockCursor();
+    }
+
+    void Update()
+    {
+        // เช็คปุ่ม ESC แบบ Input System ใหม่
+        if (Keyboard.current != null && Keyboard.current.gKey.wasPressedThisFrame)
+        {
+            ToggleCursor();
+        }
+    }
+
+    void ToggleCursor()
+    {
+        isCursorLocked = !isCursorLocked;
+
+        if (isCursorLocked)
+            LockCursor();
+        else
+            UnlockCursor();
+    }
+
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
