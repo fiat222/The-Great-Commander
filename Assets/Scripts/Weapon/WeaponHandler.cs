37f3e77c77086eafa018b75184951170fdb00a22
NaySanpipop🔥 <sanpipop9@gmail.com>
2026-02-26 17:15:14 +0700

    SmoothPlayerAnimation

--- Assets/Scripts/Weapon/WeaponHandler.cs
+++ Assets/Scripts/Weapon/WeaponHandler.cs
@@ -4,11 +4,14 @@ public class WeaponHandler : MonoBehaviour
 {
     [SerializeField] private Collider weaponHitbox;
 
+    // --- ⚔️ หมวดจัดการ Collider (ดาเมจ) ---
     public void EnableHitbox()
     {
         weaponHitbox.enabled = true;
     }
-    public void DisableHitbox() {
+
+    public void DisableHitbox() 
+    {
         weaponHitbox.enabled = false;
     }
 }
